9c353c9249c8a2bfb5d3b9cc262fc5f337c11bce
Missael <lmpadillar@gmail.com>
2026-04-05 10:29:25 -0700

    Migrate to micro services

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,36 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { envConfig } from './config';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
 
   const logger = new Logger('Bootstrap');
 
-  app.setGlobalPrefix('api');
-  app.useGlobalPipes(
-    new ValidationPipe({
-    whitelist: true,
-    forbidNonWhitelisted: true,
-    })
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: envConfig.PORT,
+      },
+    },
   );
+  await app.listen();
+  logger.log(`Server is running on port ${envConfig.PORT}`);
+  // const app = await NestFactory.create(AppModule);
+  // const logger = new Logger('Bootstrap');
 
-  await app.listen(envConfig.PORT, () => {
-    console.log(`Server is running on port ${envConfig.PORT}`);
-  });
+  // app.setGlobalPrefix('api');
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //   whitelist: true,
+  //   forbidNonWhitelisted: true,
+  //   })
+  // );
+
+  // await app.listen(envConfig.PORT, () => {
+  //   console.log(`Server is running on port ${envConfig.PORT}`);
+  // });
 }
 bootstrap();
