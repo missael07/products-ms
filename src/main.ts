153fc5913f587e371b23b9eaa553fc9ca229c6f2
Missael <lmpadillar@gmail.com>
2026-04-04 19:32:57 -0700

    Set Enviroments Variables validation and Dto validations

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { envConfig } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+    whitelist: true,
+    forbidNonWhitelisted: true,
+    })
+  );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envConfig.PORT, () => {
+    console.log(`Server is running on port ${envConfig.PORT}`);
+  });
 }
 bootstrap();
